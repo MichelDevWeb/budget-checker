890bd3d3120c9047cac6bc227651dbf1fe21bd17
MichelDevWeb <micheldevweb2020@gmail.com>
2024-10-02 22:07:28 +0700

    Init project, Implement SignIn, Wizard, Dashboard page

---
+++ lib/currencies.ts
@@ -0,0 +1,6 @@
+export const Currencies = [
+  { value: "USD", label: "$ Dollar", locale: "en-US" },
+  { value: "VND", label: "đ VND", locale: "vi-VN" },
+];
+
+export type Currency = (typeof Currencies)[0];
