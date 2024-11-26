dcaf04a68de0bdc65376731392cba52bc6508e1f
MichelDevWeb <micheldevweb2020@gmail.com>
2024-11-26 23:31:57 +0700

    Comment out

--- app/mobile-dashboard/_components/PieChartOverview.tsx
+++ app/mobile-dashboard/_components/PieChartOverview.tsx
@@ -37,13 +37,13 @@ const renderActiveShape = (props: any) => {
   } = props;
   const sin = Math.sin(-RADIAN * midAngle);
   const cos = Math.cos(-RADIAN * midAngle);
-  const sx = cx + (outerRadius + 5) * cos;
-  const sy = cy + (outerRadius + 10) * sin;
+  // const sx = cx + (outerRadius + 5) * cos;
+  // const sy = cy + (outerRadius + 10) * sin;
   const mx = cx + (outerRadius + 10) * cos;
   const my = cy + (outerRadius + 30) * sin;
   const ex = mx + (cos >= 0 ? 1 : -1) * 22;
   const ey = my;
-  const textAnchor = cos >= 0 ? "start" : "end";
+  // const textAnchor = cos >= 0 ? "start" : "end";
 
   return (
     <g>
